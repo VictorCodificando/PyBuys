7023443210c09935194005d4cb275dd72a5640e0
VictorCodificando <victorcodificando@gmail.com>
2023-04-27 21:03:23 +0200

    Añadido indice

--- src/pybuys/core/views.py
+++ src/pybuys/core/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import redirect, render
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.forms import AuthenticationForm, UserCreationForm
+from product.models import Categorias
 
 from pybuys.settings import MEDIA_URL
 
@@ -30,10 +31,11 @@ def signup(request):
 
 @login_required
 def index(request):
-    return render(request, "core/index.html")
+    categorias = Categorias.objects.filter(grupo__isnull=True)
+    return render(request, "core/index.html", {"categorias": categorias})
+
 
 @login_required
 def logout(request):
     request.session.flush()
     return render(request, "core/home.html")
-
