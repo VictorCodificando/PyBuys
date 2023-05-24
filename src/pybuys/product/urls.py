16f68d1221c82c474e2898e7d96a85bd8fd0e2ec
VictorCodificando <victorcodificando@gmail.com>
2023-05-24 16:35:15 +0200

    Eliminado todo

--- src/pybuys/product/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('detail/<int:pk>/', views.product_detail, name='detail'),
-    path('', views.productos, name='productos'),
-    path('busqueda', views.buscar_productos, name='busqueda')
-]
--- src/pybuys/utils/__init__.py
+++
@@ -1,3 +0,0 @@
-from .utils import pertenece_a_categoria
-from .utils import modificarUsuario
-from .utils import eliminar_de_carritos
